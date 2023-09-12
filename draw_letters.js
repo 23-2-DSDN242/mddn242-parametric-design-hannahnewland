4bd06529067b8d997a2cc4b45e13c4bc9f425652
hannahnewland <139179132+hannahnewland@users.noreply.github.com>
2023-09-12 20:59:51 +1200

    Completed Letters up to O

--- draw_letters.js
+++ draw_letters.js
@@ -4,13 +4,26 @@ var systemLineColor = "#03045e";
 var systemBoxColor = "#00c800";
 
 /* internal constants */
-const darkBlue  = "#0077b6";
-const lightBlue  = "#90e0ef";
-const strokeColor  = "#03045e";
+// const darkBlue  = "#0077b6";
+// const lightBlue  = "#90e0ef";
+// const strokeColor  = "#03045e";
 
-const darkOrange  = "#E94D29";
-const lightOrange  = "#F0A10B";
-const White = "#90e0tf";
+ //const darkOrange  = "#E94D29";
+// const lightOrange  = "#F0A10B";
+// const White = "#90e0tf";
+const backgroundColor  = "#F2C9E1";
+const strokeColor      = "#000000";
+
+const SfromRightBlockFill = "#E43430"; //red
+const ReverseLBlockFill = "#F49941"; //orange
+const LBlockFill = "#FFED4E"; //yellow
+const TBlockFill = "#4DB270"; //green
+const SfromLeftBlockFill = "#53BBB3" //teal
+const TwobyTwoBlockFill = "#3D8DCC" //blue
+const TwobyOneBlockFill = "#6371B5" //indigo
+const OnebyOneBlockFill = "#7962A8" //violet
+const FourbyOneBlockFill = "#ED6D9B" //pink
+
 
 /*
  * Draw the letter given the letterData
@@ -22,30 +35,238 @@ const White = "#90e0tf";
 function drawLetter(letterData) {
   // color/stroke setup
   stroke(strokeColor);
-  // strokeWeight(4);
-  noStroke();
+   strokeWeight(4);
+  
 
   // determine parameters for second circle
   
-  let posx = 0  + letterData["xposblock1"];
-  let posy = 180 + letterData["yposblock1"];
+  let posx = 0;
+  let posy = 0;
 
-  let xposblock1 = posx + letterData["xposblock1"];
-  let yposblock1 = posy + letterData["yposblock1"];
-
-  // draw two circles
-  fill(darkOrange);
-  // ellipse(pos2x, pos2y, 100, 100);
+  let Lblock = letterData ["Lblock"];
+  let Tblock = letterData ["Tblock"];
+  let ReverseLblock = letterData ["ReverseLblock"];
+  let PosX4by1 = posx + letterData ["PosX4by1"];
+  let PosX2by2 = posx + letterData ["PosX2by2"];
+  let PosY2by2 = posy + letterData ["PosY2by2"];
+  let PosX1by1 = posx +letterData ["PosX1by1"];
+  let PosY1by1 = posy +letterData ["PosY1by1"];
+  let block2by1 = letterData ["block2by1"];
+  let PosX2by1 = posx + letterData ["PosX2by1"];
+  let PosY2by1 = posy +letterData ["PosY2by1"];
+  let Multiple1by1s = letterData ["Multiple1by1s"];
+  let SfromLeftBlock = letterData ["SfromLeftBlock"];
+  let SfromRightBlock = letterData ["SfromRightBlock"];
   
-  // rect(rectX,rectY,rectW,rectH);
-  // fill(lightOrange);
-  // arc(pos2x,pos2y,size2,size2,start,stop);
-  // fill(White);
-  // ellipse(posAX,posAY, sizeCircleA);
-  // fill(darkOrange);
-  // ellipse(posBX,posBY,sizeCircleB);
 
-  rect(xposblock1,yposblock1,30,20);
+  fill(TBlockFill);
+if(Tblock == 1){
+
+beginShape();
+vertex(posx +66, posy+200);
+vertex(posx + 100, posy+200);
+vertex(posx+100,posy+100);
+vertex(posx+66, posy+100);
+vertex(posx+66, posy+133);
+vertex(posx+33,posy+133);
+vertex(posx+33,posy+166);
+vertex(posx+66,posy+166);
+vertex(posx+66,posy+200);
+endShape();
+
+}else if (Tblock == 2){
+beginShape();
+vertex(posx, posy+200);
+vertex(posx, posy+166);
+vertex(posx+33,posy+166);
+vertex(posx+33, posy+133);
+vertex(posx+66, posy+133);
+vertex(posx+66,posy+166);
+vertex(posx+100,posy+166);
+vertex(posx+100,posy+200);
+vertex(posx,posy+200);
+endShape();
+} else if (Tblock == 3){
+
+  beginShape();
+  vertex(posx, posy+200);
+  vertex(posx, posy+100);
+  vertex(posx+33,posy+100);
+  vertex(posx+33, posy+133);
+  vertex(posx+66, posy+133);
+  vertex(posx+66,posy+166);
+  vertex(posx+33,posy+166);
+  vertex(posx+33,posy+200);
+  vertex(posx,posy+200);
+  endShape();
+
+}
+
+fill(TwobyTwoBlockFill);
+rect(PosX2by2, PosY2by2, 66,66);
+
+fill(LBlockFill);
+
+if(Lblock == 1){
+  
+ beginShape();
+vertex(posx,posy+200);
+vertex(posx,posy+100);
+vertex(posx+66,posy+100);
+vertex(posx+66,posy+133);
+vertex(posx+33,posy+133);
+vertex(posx+33,posy+200);
+vertex(posx,posy+200);
+endShape();
+} else if (Lblock == 2){
+
+  beginShape();
+  vertex(posx,posy+133);
+  vertex(posx,posy+100);
+  vertex(posx+100,posy+100);
+  vertex(posx+100,posy+166);
+  vertex(posx+66,posy+166);
+  vertex(posx+66,posy+133);
+  vertex(posx,posy+133);
+  endShape();
+}else if (Lblock == 3){
+
+  beginShape();
+  vertex(posx+33,posy+200);
+  vertex(posx+33,posy+166);
+  vertex(posx+66,posy+166);
+  vertex(posx+66,posy+100);
+  vertex(posx+100,posy+100);
+  vertex(posx+100,posy+200);
+  vertex(posx+33,posy+200);
+  endShape();
+
+}else if (Lblock == 4){
+  beginShape();
+  vertex(posx,posy+200);
+  vertex(posx,posy+133);
+  vertex(posx+33,posy+133);
+  vertex(posx+33,posy+166);
+  vertex(posx+100,posy+166);
+  vertex(posx+100,posy+200);
+  vertex(posx,posy+200);
+  endShape();
+
+
+}
+
+fill(ReverseLBlockFill);
+
+if(ReverseLblock == 1){
+
+  beginShape();
+  vertex(posx+33,posy+133);
+  vertex(posx+33,posy+100);
+  vertex(posx+100,posy+100);
+  vertex(posx+100,posy+200);
+  vertex(posx+66,posy+200);
+  vertex(posx+66,posy+133);
+  vertex(posx+33,posy+133);
+  endShape();
+} else if (ReverseLblock == 2){
+  beginShape();
+  vertex(posx,posy+233);
+  vertex(posx,posy+200);
+  vertex(posx+66,posy+200);
+  vertex(posx+66,posy+166);
+  vertex(posx+100,posy+166);
+  vertex(posx+100,posy+233);
+  vertex(posx,posy+233);
+  endShape();
+
+} else if (ReverseLblock == 3){
+  beginShape();
+  vertex(posx,posy+200);
+  vertex(posx,posy+100);
+  vertex(posx+33,posy+100);
+  vertex(posx+33,posy+166);
+  vertex(posx+66,posy+166);
+  vertex(posx+66,posy+200);
+  vertex(posx,posy+200);
+  endShape();
+} else if (ReverseLblock == 4){
+  beginShape();
+  vertex(posx,posy+133);
+  vertex(posx,posy+100);
+  vertex(posx+100,posy+100);
+  vertex(posx+100,posy+133);
+  vertex(posx+33,posy+133);
+  vertex(posx+33,posy+166);
+  vertex(posx,posy+166);
+  vertex(posx,posy+100);
+  endShape();
+
+} else if (ReverseLblock == 5){
+  beginShape();
+  vertex(posx+33,posy+133);
+  vertex(posx+33,posy+100);
+  vertex(posx+100,posy+100);
+  vertex(posx+100,posy+200);
+  vertex(posx+66,posy+200);
+  vertex(posx+66,posy+133);
+  vertex(posx+33,posy+133);
+  endShape();
+
+  beginShape();
+  vertex(posx,posy+200);
+  vertex(posx,posy+100);
+  vertex(posx+33,posy+100);
+  vertex(posx+33,posy+166);
+  vertex(posx+66,posy+166);
+  vertex(posx+66,posy+200);
+  vertex(posx,posy+200);
+  endShape();
+
+  
+}
+fill(FourbyOneBlockFill);
+rect(PosX4by1,posy+66,33,133);
+
+fill(OnebyOneBlockFill);
+rect(PosX1by1, PosY1by1, 33);
+
+if(Multiple1by1s == 1){
+
+rect(0,166,33);
+rect(33,66,33);
+
+} else if (Multiple1by1s == 2){
+
+  rect(66,166,33);
+  rect(66,66,33);
+
+}
+
+fill(TwobyOneBlockFill);
+  if(block2by1 == 1){
+    rect(PosX2by1, PosY2by1,33,66);
+  }else if (block2by1 == 2){
+    rect(PosX2by1, PosY2by1,66,33);
+  }
+
+fill(SfromLeftBlockFill);
+
+if(SfromLeftBlock ==1){
+
+  beginShape();
+  vertex(posx,posy+66);
+  vertex(posx+33,posy+66);
+  vertex(posx+33,posy+100);
+  vertex(posx+66,posy+100);
+  vertex(posx+66,posy+166);
+  vertex(posx+33,posy+166);
+  vertex(posx+33,posy+133);
+  vertex(posx,posy+133);
+  vertex(posx,posy+66);
+  endShape();
+}
+  
+
 }
 
 function interpolate_letter(percent, oldObj, newObj) {
--- letters.js
+++ letters.js
@@ -5,118 +5,95 @@ const alphabet = {
     "offsety": 0
   },
   "A": {
-    // "size": 100,
-    // "offsetx": 0,
-    // "offsety": 0,
-    // "start": 115,
-    // "stop": 65,
-    // "sizeCircleA": 70,
-    // "CircleAoffsetX":0,
-    // "CircleAoffsetY":-30,
-    // "sizeCircleB": 50,
-    // "CircleBoffsetX":0,
-    // "CircleBoffsetY":-30,
-    // "rectX":-50,
-    // "rectY":0,
-    // "rectW":100,
-    // "rectH":50
-
-    "xposblock1":0,
-    "yposblock1":0,
+    "Lblock": 1,
+    "Tblock": 1, 
   },
   "B": {
-    "size": 100,
-    "offsetx": 0,
-    "offsety": 0,
-    "start": 270,
-    "stop":90,
-    "sizeCircleA": 50,
-    "CircleAoffsetX":-25,
-    "CircleAoffsetY":-25,
-    "sizeCircleB": 30,
-    "CircleBoffsetX":25,
-    "CircleBoffsetY":25,
-    "rectX":-50,
-    "rectY":-50,
-    "rectW":50,
-    "rectH":100
+    "PosX4by1": 0,
+    "PosX2by2": 33,
+    "PosY2by2": 133,
+    
   },
   "C": {
-    "size": 100,
-    "offsetx": 0,
-    "offsety": 0,
-    "start": 60,
-    "stop":300,
-    "sizeCircleA": 0,
-    "CircleAoffsetX":50,
-    "CircleAoffsetY":50,
-    "sizeCircleB":0,
-    "CircleBoffsetX":10,
-    "CircleBoffsetY":10,
-    "rectX":0,
-    "rectY":0,
-    "rectW":0,
-    "rectH":0
+    "Lblock": 1,
+    "PosX1by1": 67,
+    "PosY1by1": 100,
+    "block2by1": 2,
+    "PosX2by1": 33,
+    "PosY2by1": 167,
   },
   "D": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+   "PosX4by1":66,
+   "PosX2by2":0,
+   "PosY2by2":133,
   },
   "E": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+  "Tblock": 2,
+  "ReverseLblock":4,
   },
   "F": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "PosX4by1":0,
+    "block2by1": 2,
+    "PosX2by1": 33,
+    "PosY2by1": 66,
+    "PosX1by1": 33,
+    "PosY1by1": 133,
   },
   "G": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "PosX2by2": 0,
+    "PosY2by2": 100,
+    "block2by1": 1,
+    "PosX2by1": 66,
+    "PosY2by1": 100,
+    "ReverseLblock":2,
   },
   "H": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "Tblock": 3,
+    "block2by1": 1,
+    "PosX2by1": 66,
+    "PosY2by1": 133,
+    "PosX1by1": 0,
+    "PosY1by1": 66,
+
   },
   "I": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "block2by1": 1,
+    "PosX2by1": 33,
+    "PosY2by1": 133,
+    "PosX1by1": 33,
+    "PosY1by1": 66,
   },
   "J": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "Multiple1by1s": 1,
+    "block2by1": 1,
+    "PosX2by1": 33,
+    "PosY2by1": 133,
   },
   "K": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "SfromLeftBlock": 1,
+    "block2by1": 1,
+    "PosX2by1": 0,
+    "PosY2by1": 133,
+    "Multiple1by1s": 2,
   },
   "L": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "PosX4by1":33,
   },
   "M": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "Tblock": 3,
+    "ReverseLblock":1,
   },
   "N": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+  "Lblock":1,
+  "block2by1": 1,
+  "PosX2by1": 66,
+  "PosY2by1": 133,
+  "PosX1by1": 66,
+  "PosY1by1": 100,
   },
+
   "O": {
-    "size": 50,
-    "offsetx": 15,
-    "offsety": 0
+    "ReverseLblock": 5,
   },
   "P": {
     "size": 50,
